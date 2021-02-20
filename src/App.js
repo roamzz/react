50cf6a29fd6c6ac42bdc9e5606387ee4f21975a8
Irene <epantopoyloy@gmail.com>
2021-02-20 20:48:47 +0200

    react display menu via props and click on every dish item and get the val

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
-import React from 'react';
-// import logo from './logo.svg';
+import React, { Component } from 'react';
 import { Navbar,NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
+import "./App.css";
+import { DISHES } from './shared/dishes'
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color="primary">
-      <div className="container">
-      <NavbarBrand href="/" className="mr-auto">Restorante Con Fusion</NavbarBrand>
+class App extends Component {
 
+  constructor(props){
+    super(props);
+  
+    this.state = {
+      dishes: DISHES
+
+    };
+
+}
+
+  render(){
+    
+    return (
+      <div>
+        <Navbar dark color="warning">
+        <div className="container">
+        <NavbarBrand href="/" className="mr-auto">Restorante Con Fusion</NavbarBrand>
+  
+        </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes} />
       </div>
-      </Navbar>
-      <Menu/>
-    </div>
-  );
+    );
+
+  }
+
 }
 
 export default App;
