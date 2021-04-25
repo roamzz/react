4b1e2a4c4c0cd4b1e6264a6fa086d8581d43e1c3
Irene <epantopoyloy@gmail.com>
2021-04-25 16:44:26 +0300

    Redux Thunk

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -3,6 +3,7 @@ import { Row, Label, Col, Card, CardImg, CardText, CardBody, CardTitle, Breadcru
      Modal, ModalHeader, ModalBody} from 'reactstrap';
 import { Link } from 'react-router-dom'
 import { LocalForm, Control, Errors } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
 
 //// validators
 const required = (val) => val && val.length; //value > 0
@@ -196,7 +197,24 @@ class CommentForm extends Component {
     }
 
     const  DishDetail = (props) => {
-        if (props.dish != null) {
+        if(props.isLoading){
+            return(
+                <div className="container">
+                    <div className="row">
+                        <Loading />
+                    </div>
+                </div>
+            );
+        } else if(props.errMess){
+            return(
+                <div className="container">
+                    <div className="row">
+                       <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            );
+        }
+        else if (props.dish != null) {
             return (
                 <div className="container">
                     <div className="row">
--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -1,5 +1,7 @@
 import * as ActionTypes from './ActionTypes';
+import { DISHES } from '../shared/dishes';
 
+//add comment
 export const addComment = (dishId, rating, author, comment) => ({
     type: ActionTypes.ADD_COMMENT,
     payload: {
@@ -8,4 +10,29 @@ export const addComment = (dishId, rating, author, comment) => ({
         author: author,
         comment: comment
     }
+});
+
+//fetch disesh
+export const fetchDishes = () => (dispatch) => {
+    dispatch(dishesLoading(true));
+    setTimeout(() => {
+        dispatch(addDishes(DISHES));
+    }, 2000);
+}
+
+//dishes loading
+export const dishesLoading = () => ({
+    type: ActionTypes.DISHES_LOADING
+});
+
+//dishes failed
+export const dishesFailed = (errmess) => ({
+    type: ActionTypes.DISHES_FAILED,
+    payload:errmess
+});
+
+//add dishes
+export const addDishes = (dishes) => ({
+    type: ActionTypes.ADD_DISHES,
+    payload: dishes
 });
